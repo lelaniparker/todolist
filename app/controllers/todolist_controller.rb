5b60ece78500df392c2a071ca0d3c31f0504a8ce
lelani82 <lelaniparker@hotmail.com>
2019-07-02 16:28:30 +1000

    checkbox

--- app/controllers/todolist_controller.rb
+++ app/controllers/todolist_controller.rb
@@ -23,11 +23,26 @@ class TodolistController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.details = params[:item][:details]
+    completed = params[:item][:completed]
+  
+    # if completed is '1' set completed = true
+    if completed == '1'
+      @item.completed = true
+    else
+    # if completed is '0' set completed = false
+      @item.completed = false
+    end
+
+    # if completed.save
+    #   redirect_to todolist_index_path
+    # end
+
     if @item.save 
-      redirect_to todolist_index_path 
     else
       render 'edit'
     end
+
+    redirect_to todolist_index_path
   end
 
   def destroy
